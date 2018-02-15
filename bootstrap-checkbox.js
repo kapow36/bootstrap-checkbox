cf702ef381dda07ff3950d2dfc2e6d0dc8bafb10
kapow36 <jsphvndn@gmail.com>
2018-02-15 12:08:42 -0600

    Update bootstrap-checkbox.js

--- bootstrap-checkbox.js
+++ bootstrap-checkbox.js
@@ -1,4 +1,4 @@
-﻿(function ($)
+(function ($)
 {
     $.fn.bootstrapCheckbox = function (action, value)
     {
@@ -27,8 +27,8 @@
 
                 //generate html
                 checkbox.attr("type", "hidden");
-                checkbox.val(isChecked);
-                checkbox.wrap("<label class='btn btn-" + contextualClass + " btn-bootstrap-checkbox " + (isChecked === true ? "active" : "") + "' style='text-align:left !important;" + (isBlock === true ? " display: block; width: 100%;" : "") + "' title='" + text + "'></label>");
+                checkbox.prop("checked", isChecked);
+                checkbox.wrap("<label class='btn btn-" + contextualClass + " btn-bootstrap-checkbox " + (isChecked === true ? "" : "unchecked") + "' style='text-align:left !important;" + (isBlock === true ? " display: block; width: 100%;" : "") + "' title='" + text + "'></label>");
                 checkbox.after("&nbsp;" + text);
                 if (isChecked)
                 {
@@ -42,7 +42,7 @@
                 //click event
                 checkbox.parent().click(function (event)
                 {
-                    if (event.target.tagName === "LABEL")
+                    if (event.target.tagName !== "INPUT")
                     {
                         event.stopPropagation();
                         checkbox.bootstrapCheckbox("toggle");
@@ -55,16 +55,16 @@
             var label = this.parents(".btn-bootstrap-checkbox");
             if (value === true)
             {
-                label.addClass("active");
+                label.removeClass("unchecked");
                 label.find("span").removeClass("glyphicon-unchecked").addClass("glyphicon-check");
-                this.val(true);
+                this.prop("checked", true);
                 this.trigger("change");
             }
             else if (value === false)
             {
-                label.removeClass("active");
+                label.addClass("unchecked");
                 label.find("span").removeClass("glyphicon-check").addClass("glyphicon-unchecked");
-                this.val(false);
+                this.prop("checked", false);
                 this.trigger("change");
             }
             else
